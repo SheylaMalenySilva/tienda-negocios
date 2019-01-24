26cc78c5c294a6bd552001494e5e6bc2b2b11ffc
SheylaMalenySilva <1530140@upv.edu.mx>
2019-01-24 16:20:14 -0600

    version1

---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2019-01-22 21:33:51 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2019-01-22T21:33:51+00:00	INFO ::1	update	COM_JOOMLAUPDATE_UPDATE_LOG_DELETE_FILES
